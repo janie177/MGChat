2bf8ddf9d406d1efbfa37912994c670bc863da00
Jan <janie177@hotmail.com>
2015-05-30 05:42:01 +0200

    HQM no longer has a racetag.

--- src/main/java/com/minegusta/mgchat/tags/RaceTag.java
+++ src/main/java/com/minegusta/mgchat/tags/RaceTag.java
@@ -11,8 +11,18 @@ public class RaceTag extends PlayerTag {
         return "race";
     }
 
+    private static final String[] uuids = new String[]{"d5133464-b1ef-42b4-9ad4-8cac217d40f0", "1fb8eb3a-5431-490a-aef4-4e544c2994be"};
+
     @Override
-    public String getFor(Player player) {
+    public String getFor(Player player)
+    {
+        for(String s : uuids)
+        {
+            if(s.equals(player.getUniqueId().toString()))
+            {
+                return "";
+            }
+        }
         return Races.getRace(player).getTag();
     }
 

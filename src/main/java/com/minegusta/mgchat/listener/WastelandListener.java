f1c53e339e5cec7644b7a518e34570e7ffa50329
Jan <janie177@hotmail.com>
2015-07-12 05:39:12 +0200

    priority added.

--- src/main/java/com/minegusta/mgchat/listener/WastelandListener.java
+++ src/main/java/com/minegusta/mgchat/listener/WastelandListener.java
@@ -4,12 +4,13 @@ import com.demigodsrpg.chitchat.Chitchat;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class WastelandListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onChat(AsyncPlayerChatEvent e)
     {
         Player s = e.getPlayer();
